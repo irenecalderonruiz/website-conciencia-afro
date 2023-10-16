e4b86eb3f3862b4f4da189ab2dc9076c7e0e565f
irenecalderonruiz <irenecalderonruiz@gmail.com>
2023-10-16 15:51:37 +0200

    created scroll background-color animation

--- js/main.js
+++ js/main.js
@@ -46,31 +46,6 @@ $(document).ready(function (){
         }
     });
 
-    /* $("#btn_menu").click(function() {
-        $("body").css("overflow-y", "hidden");
-    });
-
-    $("#btn_menu").click(function() {
-        $("body").css("overflow-y", "visible");
-    });
-     */
-
-    /* $("#menu-pop-up").mouseenter(function() {
-        $("body").css("overflow-y", "hidden");
-    });
-
-    $("#menu-pop-up").mouseleave(function() {
-        $("body").css("overflow-y", "visible");
-    }); */
-
-    /* $("#menu-pop-up").on("touchstart", function() {
-        $("body").css("overflow-y", "hidden");
-    });
-    
-    $("#menu-pop-up").on("touchend", function() {
-        $("body").css("overflow-y", "visible");
-    }); */
-
     // Cambio de bg cuando clic en item de menu
         // Aparecer fondos
     $("#item-1").mouseenter(function() {
@@ -100,8 +75,39 @@ $(document).ready(function (){
         $(".menu-pop-up").removeClass('bg-img-4');
     });
 
-        
-    
+    // Cambio de bg al hacer scroll
+
+    $(window).scroll(function () {
+	    $(".container").css("background", "lightblue");
+    });
+
 });
 
+// Cambio de scroll en la página de Ayuda-Únete
+    //  Tuve que cambiar color- por bg- 
+
+$(window).scroll(function() {
+  
+    // selectors
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.panel');
+    
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+   
+    $panel.each(function () {
+      var $this = $(this);
       
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+            
+        // Quitar clases
+        $body.removeClass(function (index, css) {
+          return (css.match (/(^|\s)bg-\S+/g) || []).join(' ');
+        });
+        
+        // Añadir clases
+        $body.addClass('bg-' + $(this).data('color'));
+      }
+    });    
+    
+  }).scroll();
